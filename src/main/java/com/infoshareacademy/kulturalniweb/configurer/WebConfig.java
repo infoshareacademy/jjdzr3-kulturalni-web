6a2bef59b83d495b926be75446ff35520931569e
MaciexDrop <macwla1994@gmail.com>
2021-06-14 22:35:05 +0200

    Change webConfig - localization change works now. Deleted useless interface

--- src/main/java/com/infoshareacademy/kulturalniweb/configurer/WebConfig.java
+++ src/main/java/com/infoshareacademy/kulturalniweb/configurer/WebConfig.java
@@ -1,14 +1,16 @@
 package com.infoshareacademy.kulturalniweb.configurer;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
-
+    @ComponentScan
     @Configuration
     public class WebConfig implements WebMvcConfigurer {
 
@@ -16,7 +18,8 @@ import java.util.Locale;
         public LocaleResolver localeResolver() {
 
             SessionLocaleResolver localeResolver = new SessionLocaleResolver();
-            localeResolver.setDefaultLocale(new Locale("pl"));
+            Locale polishLocale = new Locale("pl");
+            localeResolver.setDefaultLocale(polishLocale);
             return localeResolver;
         }
 
--- src/main/java/com/infoshareacademy/kulturalniweb/configurer/WebMvcConfigurer.java
+++
@@ -1,9 +0,0 @@
-package com.infoshareacademy.kulturalniweb.configurer;
-
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-
-public interface WebMvcConfigurer {
-
-    public void addInterceptors(InterceptorRegistry registry);
-}
