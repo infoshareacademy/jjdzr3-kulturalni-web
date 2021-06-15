374791d0fe97cfd7408c8a506f9c21d61d360630
MaciexDrop <macwla1994@gmail.com>
2021-06-15 23:17:28 +0200

    Renamed controller

---
+++ src/main/java/com/infoshareacademy/kulturalniweb/controllers/FavouritesController.java
@@ -0,0 +1,26 @@
+package com.infoshareacademy.kulturalniweb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class FavouritesController {
+
+    @GetMapping("/favourites")
+    public String favourites() {
+        return "favourites";
+    }
+
+    @PostMapping("/favouritesAdd")
+    public String favouritesAdd() {
+
+        return "favourites";
+    }
+
+    @PostMapping("/favouritesRemove")
+    public String favouritesRemove() {
+
+        return "favourites";
+    }
+}
