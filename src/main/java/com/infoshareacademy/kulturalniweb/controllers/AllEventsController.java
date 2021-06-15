3e65cc1b77ec14f23f8bcec1cf39f8995d2c3d1f
PiotrR <pioru@wp.pl>
2021-06-15 14:02:54 +0200

    EventRepositoryInFile

--- src/main/java/com/infoshareacademy/kulturalniweb/controllers/AllEventsController.java
+++ src/main/java/com/infoshareacademy/kulturalniweb/controllers/AllEventsController.java
@@ -1,14 +1,23 @@
 package com.infoshareacademy.kulturalniweb.controllers;
 
+import com.infoshareacademy.kulturalniweb.domainData.EventSimple;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class AllEventsController {
 
     @GetMapping("/allEvents")
     public String allEvents (Model model) {
+        System.out.println("AllEvents");
+
+        List<EventSimple> eventSimpleList = appServiceClass.getSimpleEventsList(20);
+        model.addAttribute("collectionOfEventSimple", eventSimpleList);
+
+
         return "allEvents";
     }
 }
---
+++ src/main/java/com/infoshareacademy/kulturalniweb/repository/EventRepositoryInFile.java
@@ -0,0 +1,40 @@
+package com.infoshareacademy.kulturalniweb.repository;
+
+import com.google.gson.Gson;
+import com.infoshareacademy.kulturalniweb.domainData.EventNew;
+import org.springframework.stereotype.Component;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class EventRepositoryInFile {
+
+    private List<EventNew> eventsDB = new ArrayList<>();
+    private Path path = Paths.get("src", "main", "resources", "data.json");
+
+    public void readEventsFromGsonToList() {
+        Gson gson = new Gson();
+        try {
+            FileReader reader = new FileReader(String.valueOf(path));
+            EventNew[] eventList = gson.fromJson(reader, EventNew[].class);
+
+            for (EventNew eventNew : eventList) {
+                eventsDB.add(eventNew);
+            }
+
+/*            for (int i = 0; i < eventList.length; i++) {
+                eventsDB.add(eventList[i]);
+            }*/
+
+            System.out.println("Baza wydarzeń załadowana: + " + eventsDB.size());
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Błąd odczytu pliku .json.");
+        }
+    }
+}
