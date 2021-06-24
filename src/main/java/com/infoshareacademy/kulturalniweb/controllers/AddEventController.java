fdf86df1c37358d6e56a3bd7e4bcc78dd96c40f1
PiotrR <pioru@wp.pl>
2021-06-24 14:35:30 +0200

    addevent controller

--- src/main/java/com/infoshareacademy/kulturalniweb/controllers/AddEventController.java
+++ src/main/java/com/infoshareacademy/kulturalniweb/controllers/AddEventController.java
@@ -1,15 +1,38 @@
 package com.infoshareacademy.kulturalniweb.controllers;
 
+import com.infoshareacademy.kulturalniweb.models.NewEventDto;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 public class AddEventController {
 
     @GetMapping("/addevent")
-    public String addEvent() {
+    public String addEvent(Model model) {
+        NewEventDto newEventDto = new NewEventDto();
 
-        return "addevent";
+        model.addAttribute("newEventDto", newEventDto);
+
+        return "addeventform";
+    }
+
+
+
+
+
+    @PostMapping(value = "/saveevent")
+    public String addEvent(@ModelAttribute @Valid NewEventDto newEventDto, BindingResult result, Model model) {
+
+
+        if (result.hasFieldErrors()) {
+            return "addeventform";
+        } else {
+            return "eventsaved";
+        }
     }
 
 
