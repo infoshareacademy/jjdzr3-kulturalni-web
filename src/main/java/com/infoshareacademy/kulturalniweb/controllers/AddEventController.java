10331037779303f187ed50116a9ef7c74efd3645
MichalZajaczkowski <mzajaczkowski90@gmail.com>
2021-07-09 18:37:31 +0200

    add menu.administrationPanel

--- src/main/java/com/infoshareacademy/kulturalniweb/controllers/AddEventController.java
+++ src/main/java/com/infoshareacademy/kulturalniweb/controllers/AddEventController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AddEventController {
 
-    @GetMapping
+    @GetMapping("/addEvent")
     public String addEvent(Model model) {
         return "addeventform";
     }
