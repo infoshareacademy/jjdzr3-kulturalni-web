795e424e66cb4b0521ea214e5e8e3667270354eb
MichalZajaczkowski <mzajaczkowski90@gmail.com>
2021-07-09 18:28:32 +0200

    add addeventform templates

--- src/main/java/com/infoshareacademy/kulturalniweb/controllers/AddEventController.java
+++ src/main/java/com/infoshareacademy/kulturalniweb/controllers/AddEventController.java
@@ -1,4 +1,14 @@
 package com.infoshareacademy.kulturalniweb.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AddEventController {
+
+    @GetMapping
+    public String addEvent(Model model) {
+        return "addeventform";
+    }
 }
