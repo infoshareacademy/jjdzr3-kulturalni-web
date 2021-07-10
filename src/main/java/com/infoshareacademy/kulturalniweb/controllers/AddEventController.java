1a384bfd558c594d60ec3f86c44b4a2ce3b07a66
MichalZajaczkowski <mzajaczkowski90@gmail.com>
2021-07-10 13:43:23 +0200

    add PostMapping saveEvent in eventController

--- src/main/java/com/infoshareacademy/kulturalniweb/controllers/AddEventController.java
+++ src/main/java/com/infoshareacademy/kulturalniweb/controllers/AddEventController.java
@@ -1,8 +1,14 @@
 package com.infoshareacademy.kulturalniweb.controllers;
 
+import com.infoshareacademy.kulturalniweb.models.NewEventDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class AddEventController {
@@ -11,4 +17,15 @@ public class AddEventController {
     public String addEvent(Model model) {
         return "addeventform";
     }
+
+    @PostMapping(value = "/saveEvent")
+    public String addEvent(@ModelAttribute @Valid NewEventDto newEventDto, BindingResult result, Model model) {
+        model.addAttribute("newEventDto", newEventDto);
+        if (result.hasFieldErrors()) {
+            return "addeventform";
+        } else {
+            return "saveEvent";
+        }
+    }
 }
+
