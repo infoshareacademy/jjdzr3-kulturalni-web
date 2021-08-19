a5db84766d39b6af7703a9bd9daaa08059273362
MaciexDrop <macwla1994@gmail.com>
2021-08-19 18:43:10 +0200

    Add userController, change UserService

---
+++ src/main/java/com/infoshareacademy/kulturalniweb/controllers/UserController.java
@@ -0,0 +1,47 @@
+package com.infoshareacademy.kulturalniweb.controllers;
+
+import com.infoshareacademy.kulturalniweb.models.AppUser;
+import com.infoshareacademy.kulturalniweb.models.Token;
+import com.infoshareacademy.kulturalniweb.repository.AppUserRepository;
+import com.infoshareacademy.kulturalniweb.repository.TokenRepository;
+import com.infoshareacademy.kulturalniweb.services.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class UserController {
+
+    private TokenRepository tokenRepository;
+    private AppUserRepository appUserRepository;
+    private UserService userService;
+
+    public UserController(TokenRepository tokenRepository, AppUserRepository appUserRepository, UserService userService) {
+        this.tokenRepository = tokenRepository;
+        this.appUserRepository = appUserRepository;
+        this.userService = userService;
+    }
+    @GetMapping("/sign-in")
+    public String signUp(Model model) {
+        model.addAttribute("user", new AppUser());
+        return "sign-in";
+    }
+
+    @PostMapping("/register")
+    public String signUp(AppUser appUser) {
+        userService.addUser(appUser);
+        return "sign-in";
+    }
+
+
+    @GetMapping("/token")
+    public String sendToken(@RequestParam String value) {
+        Token byValue = tokenRepository.findByValue(value);
+        AppUser appUser = byValue.getAppUser();
+        appUser.setEnabled(true);
+        appUserRepository.save(appUser);
+        return "hello";
+    }
+}
--- src/main/java/com/infoshareacademy/kulturalniweb/services/UserService.java
+++ src/main/java/com/infoshareacademy/kulturalniweb/services/UserService.java
@@ -1,23 +1,51 @@
 package com.infoshareacademy.kulturalniweb.services;
 
+import com.infoshareacademy.kulturalniweb.models.AppUser;
+import com.infoshareacademy.kulturalniweb.models.Token;
 import com.infoshareacademy.kulturalniweb.repository.AppUserRepository;
+import com.infoshareacademy.kulturalniweb.repository.TokenRepository;
 import org.springframework.mail.MailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import java.util.UUID;
 
 @Service
 public class UserService {
 
     private MailService mailService;
-
+    private TokenRepository tokenRepository;
     private AppUserRepository appUserRepository;
     private PasswordEncoder passwordEncoder;
 
-    public UserService(MailService mailService, AppUserRepository appUserRepository, PasswordEncoder passwordEncoder) {
+    public UserService(MailService mailService, TokenRepository tokenRepository, AppUserRepository appUserRepository, PasswordEncoder passwordEncoder) {
         this.mailService = mailService;
+        this.tokenRepository = tokenRepository;
         this.appUserRepository = appUserRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
+    public void addUser(AppUser appUser) {
 
+        appUser.setPassword(passwordEncoder.encode(appUser.getPassword()));
+        appUser.setRole("ROLE_USER");
+        appUserRepository.save(appUser);
+        sendToken(appUser);
+    }
+    private void sendToken(AppUser appUser){
+        String tokenValue = UUID.randomUUID().toString();
+        Token token = new Token();
+        token.setValue(tokenValue);
+        token.setAppUser(appUser);
+        tokenRepository.save(token);
+
+        String url = "http://localhost:8080/token?value=" + tokenValue;
+
+        try {
+            mailService.sendMail(appUser.getUsername(),"Potwierdź swoje konto", url, false);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
 }
