313f4d86a79deed09f58df0903d51c1de1a5ea07
MaciexDrop <macwla1994@gmail.com>
2021-08-23 17:41:20 +0200

    Refactor http authorizeRequests

--- src/main/java/com/infoshareacademy/kulturalniweb/config/WebSecurityConfig.java
+++ src/main/java/com/infoshareacademy/kulturalniweb/config/WebSecurityConfig.java
@@ -5,12 +5,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String HOME_PAGE = "/home";
+    private static final String LOGIN_PAGE = "/sign-in";
+    private static final String LOGOUT_PAGE = "/sign-out";
+
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
@@ -34,11 +41,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.headers().disable();
         http
                 .authorizeRequests()
-                .antMatchers("/favourites", "/alleventsindex", "/addEvent", "/editEvent").authenticated()
+                .antMatchers("/favourites", "/addEvent", "/editEvent")
+                .authenticated()
                 .and()
-                .formLogin().loginPage("/loginPage").successForwardUrl("/home")
+                .formLogin()
+                .loginPage(LOGIN_PAGE)
+                .defaultSuccessUrl(HOME_PAGE)
                 .and()
-                .logout().logoutSuccessUrl("/home");
+                .logout()
+                .logoutUrl(LOGOUT_PAGE)
+                .logoutSuccessUrl(HOME_PAGE)
+                .deleteCookies("JSESSIONID");
 
 
 
