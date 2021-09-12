14e936b616fd1b42599df138c6fdbae730c8636f
MaciexDrop <macwla1994@gmail.com>
2021-09-12 17:13:42 +0200

    -home page remastered :)

--- src/main/java/com/infoshareacademy/kulturalniweb/controllers/SignInController.java
+++ src/main/java/com/infoshareacademy/kulturalniweb/controllers/SignInController.java
@@ -1,8 +1,6 @@
 package com.infoshareacademy.kulturalniweb.controllers;
 
-import com.infoshareacademy.kulturalniweb.entities.user.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
